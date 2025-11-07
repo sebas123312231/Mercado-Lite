ea526c0ea53ac95f2b1998fa2feab858b441f5d5
KathM00 <kathymontamejia@gmail.com>
2025-11-07 11:15:38 -0400

    Commits

---
+++ backend/src/entity/categoria.entity.ts
@@ -0,0 +1,21 @@
+import { Column, Entity, PrimaryGeneratedColumn, OneToMany } from "typeorm"
+import { CategoriaProducto } from "./categoriaproducto.entity" 
+
+@Entity('Categoria')
+export class Categoria {
+  @PrimaryGeneratedColumn()
+  idCategoria: number
+
+  @Column()
+  nombre: string
+  
+  @Column({ nullable: true })
+  descripcion: string
+
+  @Column({ default: true })
+  estaActiva: boolean 
+
+  // 1:N con CategoriaProducto
+  @OneToMany(() => CategoriaProducto, cp => cp.categoria)
+  categoriasProducto: CategoriaProducto[]
+}
